a9241af034e8bf188a32ce97b87331b67d7427c1
ToniLisboa <tonilisboa.souza@gmail.com>
2025-02-02 14:58:20 -0300

    Atualizei

--- landingpages/po001/script.js
+++ landingpages/po001/script.js
@@ -2,3 +2,24 @@ document.querySelector("form").addEventListener("submit", function(event) {
     event.preventDefault();
     alert("Cadastro realizado com sucesso!");
 });
+const container = document.querySelector(".carousel-container");
+const images = document.querySelectorAll(".carousel-container img");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+let index = 0;
+const totalImages = images.length;
+
+nextBtn.addEventListener("click", () => {
+    index = (index + 1) % totalImages;
+    updateCarousel();
+});
+
+prevBtn.addEventListener("click", () => {
+    index = (index - 1 + totalImages) % totalImages;
+    updateCarousel();
+});
+
+function updateCarousel() {
+    container.style.transform = `translateX(-${index * 100}%)`;
+}
